51585c652361a92ce68b73471c03a0882768d641
Amisi Deffy <amisifikirini@gmail.com>
2023-10-31 23:11:44 +0100

    adding test file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from test import nums, target
 from easy.P1_twoSum import twoSum
 
+
 if __name__ == '__main__':
-    pass
+    result = twoSum(nums, target)
+    print('*****'*10)
+    print(f'Initial array : {nums}')
+    print(f'Result : {result}')
